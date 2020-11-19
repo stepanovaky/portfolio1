e4c691d2cc0a1dbf85033a0a2b71e4ff300e3950
Katerina <psyckalla.gmail.com>
2020-11-19 11:59:19 -0500

    added punctuation

--- src/projects-helpers.js
+++ src/projects-helpers.js
@@ -8,7 +8,7 @@ export const ToDoList = {
   alt: "screenshot of to do list",
   livelink: "https://stepanovaky.github.io/to-do-list/",
   githublink: "https://github.com/stepanovaky/to-do-list",
-  p1: "Keep yourself organized with this To Do List",
+  p1: "Keep yourself organized with this To Do List.",
   p2:
     "This To Do List was created to help users organize their tasks based on level of importance.",
   p3:
@@ -25,7 +25,7 @@ export const QuizAppProject = {
   p1:
     "How well do you know King Henry VIII? Answer a few questions about him to find out!",
   p2: `This app was created to test the user's knowledge of interesting facts about King Henry VIII. It is aimed at any user interested in historical data.`,
-  p3: `I was inspired to create this quiz app from my own personal interest in Henry VIII. I loved reading about Queen Elizabeth I as a child`,
+  p3: `I was inspired to create this quiz app from my own personal interest in Henry VIII. I loved reading about Queen Elizabeth I as a child.`,
   p4: `I used HTML, CSS, JavaScript and jQuery to create this quiz.`,
 };
 
@@ -35,8 +35,8 @@ export const PurringTails = {
   alt: "screenshot of Purring Tails",
   livelink: "https://purringtails-frontend-1hevkoei8.vercel.app/login",
   githublink: "https://github.com/stepanovaky/purringtails-frontend",
-  p1: `At Purring Tails you can schedule one of four available services for your pet`,
-  p2: `This app was created to give the user a simple solution for booking one of the Purring Tails services`,
-  p3: `I was inspired to create this app from my own experiences working as a pet sitter`,
-  p4: `I used HTML, CSS, JavaScript, React.js, Node.js, Express.js, and deployed to Vercel and Heroku`,
+  p1: `At Purring Tails you can schedule one of four available services for your pet.`,
+  p2: `This app was created to give the user a simple solution for booking one of the Purring Tails services.`,
+  p3: `I was inspired to create this app from my own experiences working as a pet sitter.`,
+  p4: `I used HTML, CSS, JavaScript, React.js, Node.js, Express.js, and deployed to Vercel and Heroku.`,
 };
